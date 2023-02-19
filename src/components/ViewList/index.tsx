878f10352858be83552143e84e820dda98f59b51
Leonardo Viva <leonardo.leal@konecty.com>
2023-02-19 02:35:07 -0300

    feat: start permission drawer

--- src/components/ViewList/index.tsx
+++ src/components/ViewList/index.tsx
@@ -2,15 +2,18 @@ import { Stack } from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import xor from 'lodash/xor';
-import React, { Fragment, useCallback, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { ViewSection } from '../../DAL/Modules';
 import FieldsList from './FieldsList';
 import MyAccordionSummary from './MyAccordionSummary';
 
 const ViewList: React.FC<{ viewSections: ViewSection[] }> = ({ viewSections }) => {
 	const [selected, setSelected] = useState<string[]>([]);
+	const [isDrawerOpen, setDrawerOpen] = useState(false);
+	const drawerRef = useRef<HTMLDivElement>();
 
 	const onFieldClick = useCallback(
 		(name: string) => () => {
@@ -21,10 +24,20 @@ const ViewList: React.FC<{ viewSections: ViewSection[] }> = ({ viewSections }) =
 
 	const isSelected = useCallback((name: string) => selected.includes(name), [selected]);
 
+	useEffect(() => {
+		if (selected.length > 0) setDrawerOpen(true);
+		else setDrawerOpen(false);
+	}, [selected]);
+
+	const accordionOffset = useMemo(
+		() => (drawerRef && isDrawerOpen ? (drawerRef.current?.clientWidth || 0) * 0.7 : 0),
+		[isDrawerOpen, drawerRef],
+	);
+
 	return (
 		<Fragment>
 			{viewSections.map(({ label, icon, fields }) => (
-				<Accordion elevation={0} square disableGutters sx={{ paddingBottom: 2 }}>
+				<Accordion elevation={0} square disableGutters sx={{ paddingBottom: 2, paddingRight: `${accordionOffset}px` }}>
 					<MyAccordionSummary>
 						<Stack direction="row" spacing={3}>
 							<Typography color="primary.main">{label}</Typography>
@@ -37,6 +50,15 @@ const ViewList: React.FC<{ viewSections: ViewSection[] }> = ({ viewSections }) =
 					</AccordionDetails>
 				</Accordion>
 			))}
+			<Drawer open={isDrawerOpen} anchor="right" variant="persistent">
+				<Box display="flex" width="30vw" height="100%" ref={drawerRef}>
+					<Stack spacing={0} position="relative" width="100%">
+						<Box position="absolute" bottom={0} width="100%">
+							Hey
+						</Box>
+					</Stack>
+				</Box>
+			</Drawer>
 		</Fragment>
 	);
 };
