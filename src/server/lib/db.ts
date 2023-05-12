e47eb79914748e8957d9cb706b0b05cd333d5a89
Leonardo Viva <leonardo.leal@konecty.com>
2023-05-12 13:18:40 -0300

    feat: connect to mongodb

---
+++ src/server/lib/db.ts
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+const MONGO_DB = process.env.MONGO_DB || 'foxter';
+
+let client: MongoClient | null = null;
+
+export default async function getDB() {
+	if (client != null) {
+		return client.db(MONGO_DB);
+	}
+
+	client = await MongoClient.connect(MONGO_URL, { directConnection: true });
+	client.on('topologyClosed', () => (client = null));
+	client.on('close', () => (client = null));
+
+	return client.db(MONGO_DB);
+}
+
+const closeClient = async () => {
+	if (client != null) {
+		await client.close();
+	}
+};
+
+process.on('exit', closeClient);
+process.on('SIGINT', closeClient);
+process.on('SIGKILL', closeClient);
