4925794c81d1fd8660eea687f255dc9426031910
Leonardo Viva <leonardo.leal@konecty.com>
2024-05-27 17:20:02 -0300

    fix: memoize lazy icon

--- src/components/LazyIcon.tsx
+++ src/components/LazyIcon.tsx
@@ -1,5 +1,6 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useMemo } from "react";
 
+import { cn } from "@/lib/utils";
 import { LucideProps, PersonStanding } from "lucide-react";
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 
@@ -7,11 +8,13 @@ interface IconProps extends Omit<LucideProps, "ref"> {
   name: string;
 }
 
+type IconKeys = keyof typeof dynamicIconImports;
+
 export default function LazyIcon({ name, ...props }: IconProps) {
-  const LucideIcon = lazy(dynamicIconImports[name as keyof typeof dynamicIconImports] ?? dynamicIconImports["person-standing"]) ?? <PersonStanding />;
+  const LucideIcon = useMemo(() => lazy(dynamicIconImports[name as IconKeys] ?? dynamicIconImports["person-standing"]) ?? <PersonStanding />, [name]);
 
   return (
-    <Suspense fallback={<div className="h-1 w-1 bg-gray" />}>
+    <Suspense fallback={<div className={cn("h-1 bg-gray", props.size && `w-[${Number(props.size) * 3}px]`)} />}>
       <LucideIcon {...props} />
     </Suspense>
   );
